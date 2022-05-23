8599bd3e9e53b8ad85dada83bf781902347acfa1
Mateo <mateom.realtor@gmail.com>
2022-05-22 22:53:28 -0400

    Updated Js

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,38 @@
-console.log("nfrnfir")
+    // Grabbing the Search Button from HTML
+let searBtn = document.querySelector("#searchBtn")
+searBtn.addEventListener('click',search )
+
+let seachInputField = document.querySelector("#citySearchh")
+
+    // Parent Element
+let appenedEl = document.querySelector("#appendEl")
+
+function search(){
+     event.preventDefault();
+  console.log(seachInputField.value);
+
+    let listItemEl = document.createElement("li");
+    listItemEl=seachInputField.value
+
+appenedEl.append(listItemEl)
+
+
+
+
+
+
+  
+//   listItemEl.textContent ='WORKS'
+
+//   
+}
+
+
+
+
+
+// function grabInputVaulue (e){
+//    
+//     seachInputField = e.target.value
+    
+// }
